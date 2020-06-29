71ec28607c4bba908352cb9be5912ee3c813ed5f
Enildo Amazonas  Magno <44631463+Enildo-Amazonas-Magno@users.noreply.github.com>
2020-06-29 05:16:12 -0300

    Add files via upload

---
+++ Bombons.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int a1, a2, a3, a4, a5, a6;
+    scanf("%d" "%d" "%d" "%d" "%d" "%d", &a1, &a2, &a3, &a4, &a5, &a6 );
+    printf("Valor: R$%d.00", ((a1*a4)+(a2*a5)+(a3*a6)));
+    return 0;
+}
